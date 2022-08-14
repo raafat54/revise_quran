580cfa548b4617a66c3e571279a2ebd0ec0efb95
aburaafat <raafat.19@outlook.com>
2022-08-14 15:06:53 +0200

    update menu icon

--- app/src/main/java/com/raafat/revise/MainActivity.kt
+++ app/src/main/java/com/raafat/revise/MainActivity.kt
@@ -7,7 +7,6 @@ import android.content.pm.PackageManager
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
-import android.view.MenuItem
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
