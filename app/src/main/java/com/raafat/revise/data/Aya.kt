ebd78b841e2c49c31ba579b91917a1e979b4887a
aburaafat <raafat.19@outlook.com>
2022-10-19 17:55:39 +0200

    enhanced ux/ui

--- app/src/main/java/com/raafat/revise/data/Aya.kt
+++ app/src/main/java/com/raafat/revise/data/Aya.kt
@@ -10,8 +10,18 @@ data class Aya(
     val ayaNo: Int,
     @SerializedName("aya_text")
     val ayaText: String,
+    @SerializedName("aya_text_emlaey")
+    val ayaTextEmlaey: String,
     @SerializedName("id")
     val id: Int,
+    @SerializedName("jozz")
+    val jozz: Int,
+    @SerializedName("page")
+    val page: Int,
     @SerializedName("sura_no")
-    val sora: Int
+    val sora: Int,
+    @SerializedName("sura_name_ar")
+    val soraNameAr: String,
+    @SerializedName("sura_name_en")
+    val soraNameEn: String,
 )
